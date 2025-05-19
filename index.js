079d7ca98d8d5277ffdbac8368ab9e84864eb994
Abmir <abmirrojas15@gmail.com>
2025-05-19 14:20:02 +0200

    se agrego las dependencias de seguridad y tambien se actualizo el index.js para que las use

--- index.js
+++ index.js
@@ -2,23 +2,61 @@ import express from 'express';
 import pg from 'pg';
 import bodyParser from 'body-parser';
 import ejs from 'ejs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import bcrypt from 'bcrypt';
+import passport from 'passport';
+import { Strategy } from 'passport-local';
+import session from 'express-session';
+
+// Detalles de la sesion
 
 const app = express();
 const port = 3000;
+const saltRounds = 11;
+env.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Dependencias de sesion y paso de datos
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/login", async (req, res) => {
-  res.sendFile(path.join(__dirname, "login.html"));
+// Configuracion de la base de datos
+
+/*
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+db.connect();
+*/
+
+// Rutas de la aplicacion
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
 });
 
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
