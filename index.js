292a8acb3ea9e4cae7c96d03521b3b0981658ce3
Abmir <abmirrojas15@gmail.com>
2025-05-19 13:43:55 +0200

    se agrego la ruta de login y la estructura basica del proyecto, tambien se agrego la dependencia de ejs

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 import express from 'express';
 import pg from 'pg';
 import bodyParser from 'body-parser';
+import ejs from 'ejs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get("/login", async (req, res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
